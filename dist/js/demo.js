468692c6eeb613e23f626e9e584ed0a5e8e551dd
Jezwin <edwin.eddu.8@gmail.com>
2019-07-29 13:25:06 +0530

    Solved issues with div positions

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -6,7 +6,7 @@ $(window).scroll(function (e) {
 
 function parallax() {
   var scrolled = $(window).scrollTop();
-  $('.hero').css('top', -(scrolled * 0.0315) + 'rem');
+  $('.hero').css('top', +(scrolled * 0.0315) + 'rem');
 }
 
 ;
--- js/demo.js
+++ js/demo.js
@@ -4,6 +4,6 @@ $(window).scroll(function(e){
 
 function parallax(){
   var scrolled = $(window).scrollTop();
-  $('.hero').css('top',-(scrolled*0.0315)+'rem');
+  $('.hero').css('top',+(scrolled*0.0315)+'rem');
 
 };
